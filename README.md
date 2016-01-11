01fb25d08d9f2ae3a289430cbd12e5ac985f5e30
Enmanuel Medina <enmanuelm19@gmail.com>
2016-01-11 14:59:17 -0430

    Agregando pruebas y configurando devise

--- README.md
+++ README.md
@@ -1,12 +1,30 @@
 # Aplicacion para probar gemas pundit, devise, acts_as_votable y bourbon
 
 * Comandos de inicio
-
+* Tests
+* Devise
 #### Comandos de inicio
   Para la creacion de la aplicacion e inicializacion de rspec
 
   > rails new pundit-app
   > rails g rspec:install
+
+#### Tests
+
+  Creamos nuestro primer test para el sign_in *spec/features/sign_in_spec.rb* y
+  un helper que realizara las acciones por nosotros en *spec/support/features/session_helper.rb*.
+
+  Configuramos el *rails_helper.rb* para que acepte el *session_helper.rb* con la siguiente linea
+
+  > config.include Features::SessionHelpers, type: :feature
+
+#### Devise
+
+  Generamos e instalamos devise con el siguiente comando:
+
+  > rails g devise:install
+
+  
 
 Things you may want to cover:
 
---
+++ spec/features/sign_in_spec.rb
@@ -0,0 +1,26 @@
+require "rails_helper"
+
+  feature 'Sign in', :devise do
+    scenario 'Usuario no puede iniciar sesion si no esta registrado' do
+      signin('prueba1@example.com', 'password')
+      expect(page).to have_content 'Invalid email or password.'
+    end
+
+    scenario 'Usuario puede iniciar sesion con credenciales validas' do
+      user = FactoryGirl.create(:user)
+      signin(user.email, user.password)
+      expect(page).to have_content 'Sign in successfully.'
+    end
+
+    scenario 'Usuario no puede iniciar sesion con email invalido' do
+      user = FactoryGirl.create(:user)
+      signin('invalid@email.com', user.password)
+      expect(page).to have_content 'Invalid email or password.'
+    end
+
+    scenario 'Usuario no puede iniciar sesion con password invalido' do
+      user = FactoryGirl.create(:user)
+      signin(user.email, 'invalidpassword')
+      expect(page).to have_content 'Invalid email or password.'
+    end
+  end
