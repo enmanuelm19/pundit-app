8505adbf66169de7ad6e82ecf4f8d9a92d084ffb
Enmanuel Medina <enmanuelm19@gmail.com>
2016-01-11 17:15:40 -0430

    Pagina index de usuarios configurada

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,10 @@
 FactoryGirl.define do
   factory :user do
-    sequence(:email) { |n| "person_#{n}@example.com"} 
+    sequence(:email) { |n| "person_#{n}@example.com"}
     password 'password'
+
+    trait :admin do
+      role 'admin'
+    end
   end
 end
